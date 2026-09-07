db6540882bac125eb6126425a356445583ce2d52
davemayor2 <davidolajohn.com>
2026-09-07 09:52:21 +0100

    Move typescript to production dependencies for cPanel build

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
